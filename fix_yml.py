c33e0b80c2e42d46437f3bd97d9ebb1bbaba3f49
swalker152 <stacy.walker152@t-mobile.com>
2021-03-24 11:02:26 -0700

    accessing repo trees

--- fix_yml.py
+++ fix_yml.py
@@ -1,10 +1,18 @@
 import gitlab
 
 # private token auth
-gl = gitlab.Gitlab("https://gitlab.com", private_token="")
-gl.auth()
+with gitlab.Gitlab("https://gitlab.com", private_token="") as gl:
+    gl.auth()
 
-# list projects in a given group
-group = gl.groups.get(7005886)
-for project in group.projects.list(as_list = False):
-    print(project.name)
+    # list projects in a given group
+    group = gl.groups.get(7005886)
+    for project in group.projects.list(as_list = False):
+        # print(project.name, project.id, project.ssh_url_to_repo)
+    # reporting = gl.projects.get(16633661)
+    # print(reporting)
+    # print(type(reporting))
+    # tree_list = reporting.repository_tree(recursive = True, all = True)
+    # for item in tree_list:
+    #     # print(item)
+    #     if item["name"] == ".gitlab-ci.yml":
+    #         print(item["name"], item["path"])
